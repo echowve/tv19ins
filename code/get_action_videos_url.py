0f8f44a64b45222d91c42c0075c88cb0b1c34ddf
jlx <jianglx@whu.edu.cn>
2019-04-16 20:00:10 +0800

    add url generator of action videos

---
+++ code/get_action_videos_url.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# you can use this script to generate urls of all topic action videos
+# the you can download them with uget software on ubuntu
+
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    with open("ins.action.topics") as f:
+    	lines = f.readlines()
+    	for line in lines:
+    		file = line.split(" ")[1][:-1] + ".mp4"
+    		remote = 'https://www-nlpir.nist.gov/projects/tv2019/active/INS/topics/actions.examples/' +  file
+    		print(remote)
+
+
